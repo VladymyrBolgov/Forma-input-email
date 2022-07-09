8c1e921d431a2a4fc1c78c595988e39be0930d42
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-09 20:52:28 +0300

    forma

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,15 @@
-/*//1 вариант простая
-const openBtn = document.querySelector(".open");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".close");
+const form = document.querySelector(".form");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
 
-/*function showModal () {/ / hide в css скрывает модалку, удаляя hide
-  overlay.classList.remove('hide') // открывается модалка
-  }
-function hideModal() {
-  overlay.classList.add('hide');
-   }*/
-/*function toggleModal() { //toggleModal открыть и закрыть
-    overlay.classList.toggle('hide')
-  }
-openBtn.addEventListener('click', toggleModal);
-closeBtn.addEventListener('click', toggleModal);*/
-
-// Второй вариант с клавиатурой
-const openBtn = document.querySelector(".open");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".close");
-
-function toggleModal(event) {
-  const { code } = event;
-  if (code === 'Escape') {
-    if (overlay.classList.contains('hide')) {
-      return;
-    }
-    overlay.classList.toggle('hide');
-    return;
-  }
-  overlay.classList.toggle('hide');
+function handleSubmit(e) {
+  e.preventDefault();
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+  form.reset();
 }
-openBtn.addEventListener('click', toggleModal);
-closeBtn.addEventListener('click', toggleModal);
 
-addEventListener('keydown', toggleModal);
+form.addEventListener("submit", handleSubmit);
